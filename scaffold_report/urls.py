49f61518a325cb3ff71367261753707ced2a13ee
khchine5 <hamzakhchine@gmail.com>
2017-05-15 11:59:10 +0100

    Use new url definition

--- scaffold_report/urls.py
+++ scaffold_report/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import *
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from scaffold_report import views
 from scaffold_report import report
 
 report.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url('^(?P<name>\w+)/$', login_required(views.ScaffoldReportView.as_view()), name='scaffold-report'),
     url('^(?P<name>\w+)/view/$', login_required(views.DownloadReportView.as_view()), name='scaffold-report-download'),
-    )
+]
